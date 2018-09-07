5d6d831618342a30b2a6e3b9dbd838436767051c
EC2 Default User <ec2-user@ip-172-31-8-195.us-east-2.compute.internal>
2018-09-07 17:31:12 +0000

    test git

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,16 +2,30 @@
 
 # Part 1
 
+
 def sum arr
   # YOUR CODE HERE
+  return arr.inject(0, :+)
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length==0
+    return 0
+  elsif arr.length==1
+    return arr[0]
+  else
+    return arr.sort.last(2).sum
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.length==0 || arr.length ==1
+    return FALSE
+  else
+    return arr.combination(2).any?{|a,b| a+b==n}
+  end
 end
 
 # Part 2
